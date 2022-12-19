c18f9f4c8a76402d20e8fc47e960954aeb00aa07
Solomon Wakhungu <1solomonwakhungu@gmail.com>
2022-12-19 14:24:21 -0600

    build: Pass test five
    This test is about summing all the elements in between
    two values. The values might appear in different types other
    than numbers and the code had to handle those cases.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    args = args.sort();
+    debugger;
+    if(args[0] < 0){
+        return "ERROR";
+    } 
 
+    if(typeof args[0] === 'number' && typeof args[1] === 'number'){
+        let sum = 0;
+        for(let i = args[0]; i <= args[1]; i++){
+            sum += i;
+        }
+
+        return sum;
+    }
+
+    return "ERROR";
 };
 
 // Do not edit below this line
