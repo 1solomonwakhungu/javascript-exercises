6c5ea83359a3d587a9c84c5276e094d7f70d2e14
Solomon Wakhungu <1solomonwakhungu@gmail.com>
2022-12-19 13:59:27 -0600

    build: Pass the fourth test
    The fourth test is about arrays and splicing arrays
    in order to remove unwanted elements from the array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elements) {
+    // Loop through the elements
+    for(const elem of elements){
+        let index = arr.indexOf(elem);
+        if(index != -1){
+            arr.splice(index, 1);
+        }
+    }
 
+    return arr;
 };
 
 // Do not edit below this line
