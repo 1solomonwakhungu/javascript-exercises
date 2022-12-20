b06007f3e6b69897f6d62c0059145b59e9d80cf1
Solomon Wakhungu <1solomonwakhungu@gmail.com>
2022-12-20 13:00:27 -0600

    build: Pass the seventh exercise
    This exercise was about using more complex equations in
    Javascript and rounding in Javascript.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(faren) {
+  let calc = (faren - 32) * (5/9);
+  return Number(calc.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let calc = (cel * (9/5) + 32);
+  return Number(calc.toFixed(1));
 };
 
 // Do not edit below this line
